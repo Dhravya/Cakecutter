e1fa70eb3b93bb2848438c75d36d82343fe8626f
Dhravya <dhravyashah@gmail.com>
2022-03-08 22:47:37 +0530

    added content addition

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-use std::fs::{self, File, create_dir};
+use std::{
+    fs::{self, create_dir, File},
+    io::Write,
+};
 use toml::Value;
 
 fn main() {
     // Gets the Cake.toml file
-    let toml = fs::read_to_string("Cake.toml")
-        .expect("Could not read the Cake.toml file.");
+    let toml = fs::read_to_string("Cake.toml").expect("Could not read the Cake.toml file.");
     let toml = toml.parse::<Value>().unwrap();
-    
+
     let filestructure = toml["filestructure"].as_table().unwrap();
+    let content = toml["content"].as_table().unwrap();
 
     for (key, value) in filestructure {
         create_dir(key.as_str()).unwrap();
 
         for file in value.as_array().unwrap() {
             let file = file.as_str().unwrap();
-            File::create(format!("{}/{}", key, file)).unwrap();
+            let mut file = File::create(format!("{}/{}", key, file)).unwrap();
+
+            let content_key = format!(
+                "{}__{}",
+                key,
+                value.as_array().unwrap()[0].as_str().unwrap()
+            );
+            let content = content[content_key.as_str()].as_str().unwrap();
+            file.write_all(content.as_bytes()).unwrap();
         }
     }
-} 
+}
